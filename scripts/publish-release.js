7516d4d6656abb8ea9530b2326fc6c0b7dcfee4e
panupong.jab <panupong.kra@turbo.co.th>
2025-07-28 00:55:56 +0700

    fix wrong update publish file

--- scripts/publish-release.js
+++ scripts/publish-release.js
@@ -1,9 +1,10 @@
 import{ execSync } from 'child_process'
 import fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const packageJsonPath = path.resolve(__dirname, '../package.json');
 
 function getPackageVersion() {
