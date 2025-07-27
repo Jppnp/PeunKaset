508d84686ff1193a4c2bdd56e91ebf53c307e6fc
panupong.jab <panupong.kra@turbo.co.th>
2025-07-28 00:56:46 +0700

    update publish file finally

--- scripts/publish-release.js
+++ scripts/publish-release.js
@@ -1,8 +1,9 @@
 import{ execSync } from 'child_process'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const packageJsonPath = path.resolve(__dirname, '../package.json');
